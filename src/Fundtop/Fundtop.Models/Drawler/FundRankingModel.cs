c6ef595a11d0287227e484e24d8d85bb1bcea634
reggieqiao <reggieqiao@gmail.com>
2024-01-30 14:52:48 +0800

    feat:add FundRankingModel

---
+++ src/Fundtop/Fundtop.Models/Drawler/FundRankingModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Fundtop.Models.Drawler
+{
+    public class FundRankingModel
+    {
+        public int Id { get; set; }
+        public string FundCode { get; set; }
+        public string FundShortName { get; set; }
+        public DateTime? Date { get; set; }
+        public decimal NetAssetValue { get; set; }
+        public decimal CumulativeNetAssetValue { get; set; }
+        public decimal DailyGrowthRate { get; set; }
+        public decimal OneWeek { get; set; }
+        public decimal OneMonth { get; set; }
+        public decimal ThreeMonth { get; set; }
+        public decimal SixMonth { get; set; }
+        public decimal OneYear { get; set; }
+        public decimal TwoYear { get; set; }
+        public decimal ThreeYear { get; set; }
+        public decimal YearToDate { get; set; }
+        public decimal SinceInception { get; set; }
+        public DateTime? InceptionDate { get; set; }
+        public string TransactionFee { get; set; }
+    }
+}
