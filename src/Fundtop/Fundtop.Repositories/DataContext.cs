7e5cd252b223c293034ba0b78614e9f0fac5178c
reggieqiao <reggieqiao@gmail.com>
2024-01-31 13:24:47 +0800

    feat:add Fundtop.Repositories project and DataContext

---
+++ src/Fundtop/Fundtop.Repositories/DataContext.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.SQLite;
+
+namespace Fundtop.Repositories
+{
+    public class DataContext : IDisposable
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IDbConnection _connection;
+
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+            var databaseDefault = _configuration["Database:Default"];
+            var databaseConnection = _configuration[$"Database:Connection:{databaseDefault}"];
+            switch (databaseDefault.ToUpper())
+            {
+                case "MYSQL":
+                    _connection = new MySqlConnection(databaseConnection);
+                    break;
+                case "SQLITE":
+                    _connection = new SQLiteConnection(databaseConnection);
+                    break;
+                default:
+                    throw new NotSupportedException($"Database type {databaseDefault} not supported.");
+            }
+
+            try
+            {
+                _connection.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Database connection error:{ex.Message}");
+            }
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return _connection;
+        }
+
+        public void Dispose()
+        {
+            if (_connection.State != ConnectionState.Closed)
+            {
+                _connection.Close();
+            }
+            _connection.Dispose();
+        }
+    }
+}
