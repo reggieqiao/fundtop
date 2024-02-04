dd7d05b795fcf60ccda9d5c400ad46e369a3f684
reggieqiao <reggieqiao@gmail.com>
2024-02-04 11:40:52 +0800

    feat:add AutoMapperHelper

---
+++ src/Fundtop/Fundtop.Core/AutoMapperHelper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Fundtop.Core
+{
+    public static class AutoMapperHelper
+    {
+        public static IEnumerable<TDestination> MapList<TSource, TDestination>(IEnumerable<TSource> sourceList)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TSource, TDestination>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<IEnumerable<TDestination>>(sourceList);
+        }
+    }
+}
