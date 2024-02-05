64a8df8353e80bbc0ac76a1bd2c6777e72749973
reggieqiao <reggieqiao@gmail.com>
2024-02-05 15:07:33 +0800

    feat:add PagedResult

---
+++ src/Fundtop/Fundtop.Models/Web/PagedResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Fundtop.Models.Web
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalPages
+        {
+            get 
+            { 
+                return (int)Math.Ceiling(TotalCount / (double)PageSize); 
+            }
+        }
+        
+    }
+}
