2baddb2737e4907252f9fa0b33f81a64b588a40f
reggieqiao <reggieqiao@gmail.com>
2024-02-08 21:34:06 +0800

    feat:add FundRankingModel

---
+++ src/Fundtop/Fundtop.Models/Web/FundRankingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fundtop.Models.Web
+{
+    public class FundRankingModel
+    {
+        public int PageIndex { get; set; } = 1;
+
+        public int PageSize { get; set; } = 100;
+    }
+}
