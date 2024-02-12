9ecd2e4dace1065e53d1c65bf59b1987e9696501
reggieqiao <reggieqiao@gmail.com>
2024-02-12 14:37:50 +0800

    feat:add IFundRankingRepository

---
+++ src/Fundtop/Fundtop.Repositories/Web/Interface/IFundRankingRepository.cs
@@ -0,0 +1,9 @@
+﻿using Fundtop.Models.Web;
+
+namespace Fundtop.Repositories.Web.Interface
+{
+    public interface IFundRankingRepository
+    {
+        Task<PagedResult<FundRankingDto>> GetFundListAsync(FundRankingModel model);
+    }
+}
