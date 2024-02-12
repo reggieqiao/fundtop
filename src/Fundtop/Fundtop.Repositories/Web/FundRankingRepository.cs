af0d18e01f755d1f72ced18a145ce02e73557da3
reggieqiao <reggieqiao@gmail.com>
2024-02-13 00:01:34 +0800

    feat:add FundRankingRepository

---
+++ src/Fundtop/Fundtop.Repositories/Web/FundRankingRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using Fundtop.Core;
+using Fundtop.Models.Web;
+using Fundtop.Repositories.Web.Interface;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Fundtop.Repositories.Web
+{
+    public class FundRankingRepository : IFundRankingRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IDbConnection _connection;
+
+        public FundRankingRepository(IConfiguration configuration, DataContext connection)
+        {
+            _configuration = configuration;
+            _connection = connection.GetConnection();
+        }
+
+        public async Task<PagedResult<FundRankingDto>> GetFundListAsync(FundRankingModel model)
+        {
+            var pageNumber = (model.PageIndex - 1) * model.PageSize;
+            var sql = @"
+                SELECT *, COUNT(*) OVER() AS total_count
+                FROM fund_ranking
+                ORDER BY id
+                LIMIT @PageSize OFFSET @PageNumber";
+            var result = await _connection.QueryAsync<FundRankingEntity>(sql, new { PageNumber = pageNumber, PageSize = model.PageSize });
+            var items = AutoMapperHelper.MapList<FundRankingEntity, FundRankingDto>(result);
+            var totalCount = 0;
+            if (result.Any())
+            {
+                var first = result.FirstOrDefault();
+                if (first != null)
+                {
+                    totalCount = first.TotalCount;
+                }
+            }
+
+            var pagedResult = new PagedResult<FundRankingDto> { 
+                Items = items,
+                TotalCount = totalCount,
+                PageIndex = model.PageIndex,
+                PageSize = model.PageSize
+            };
+            return pagedResult;
+        }
+    }
+}
