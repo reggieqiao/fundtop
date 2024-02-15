d5fc130cabacf18d475c348228f6325b98c79794
reggieqiao <reggieqiao@gmail.com>
2024-02-16 00:10:00 +0800

    feat:add FundRankingService

---
+++ src/Fundtop/Fundtop.Web/Services/FundRankingService.cs
@@ -0,0 +1,22 @@
+﻿using Fundtop.Models.Web;
+using Fundtop.Repositories.Web.Interface;
+using Fundtop.Web.Services.Interface;
+
+namespace Fundtop.Web.Services
+{
+    public class FundRankingService : IFundRankingService
+    {
+        private readonly IFundRankingRepository _fundRankingRepository;
+
+        public FundRankingService(IFundRankingRepository fundRankingRepository)
+        {
+            _fundRankingRepository = fundRankingRepository;
+        }
+
+        public async Task<PagedResult<FundRankingDto>> GetFundListAsync(FundRankingModel model)
+        {
+            var result = await _fundRankingRepository.GetFundListAsync(model);
+            return result;
+        }
+    }
+}
