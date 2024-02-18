f0f30cb65fb6975580e6180ef0f2650620140866
reggieqiao <reggieqiao@gmail.com>
2024-02-19 00:25:22 +0800

    feat:add HomeController

---
+++ src/Fundtop/Fundtop.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Fundtop.Models.Web;
+using Fundtop.Web.Models;
+using Fundtop.Web.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fundtop.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IFundRankingService _fundRankingService;
+
+        public HomeController(ILogger<HomeController> logger, IFundRankingService fundRankingService)
+        {
+            _logger = logger;
+            _fundRankingService = fundRankingService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> FundListAsync(FundRankingModel model)
+        {
+            var result = await _fundRankingService.GetFundListAsync(model);
+            return Ok(ApiResponse<PagedResult<FundRankingDto>>.Success(result));
+        }
+    }
+}
