7194af8ba9ab9e4b6d08d4c46cf725080974caf1
reggieqiao <reggieqiao@gmail.com>
2024-04-06 15:10:09 +0800

    feat:add keywords

--- src/Fundtop.Models/Web/FundRankingModel.cs
+++ src/Fundtop.Models/Web/FundRankingModel.cs
@@ -2,6 +2,8 @@
 {
     public class FundRankingModel
     {
+        public string Keywords { get; set; }
+
         public int PageIndex { get; set; } = 1;
 
         public int PageSize { get; set; } = 100;
