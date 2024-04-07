f81ab0fe0189fbfdffbe6f06290e872667b3f38e
reggieqiao <reggieqiao@gmail.com>
2024-04-07 21:38:52 +0800

    fix:fund list search

--- src/Fundtop.Repositories/Web/FundRankingRepository.cs
+++ src/Fundtop.Repositories/Web/FundRankingRepository.cs
@@ -4,6 +4,7 @@ using Fundtop.Models.Web;
 using Fundtop.Repositories.Web.Interface;
 using Microsoft.Extensions.Configuration;
 using System.Data;
+using System.Text;
 
 namespace Fundtop.Repositories.Web
 {
@@ -21,10 +22,15 @@ namespace Fundtop.Repositories.Web
         public async Task<PagedResult<FundRankingDto>> GetFundListAsync(FundRankingModel model)
         {
             var pageNumber = (model.PageIndex - 1) * model.PageSize;
-            var sql = @"
+            var condition = new StringBuilder();
+            if (!string.IsNullOrWhiteSpace(model.Keywords))
+            {
+                condition.Append($"AND (fund_code LIKE '%{model.Keywords}%' OR fund_shortname LIKE '%{model.Keywords}%')");
+            }
+            var sql = @$"
                 SELECT *, COUNT(1) OVER() AS total_count
                 FROM fund_ranking
-                WHERE DATE(created_at)=(SELECT DATE(MAX(created_at)) FROM fund_ranking)
+                WHERE DATE(created_at)=(SELECT DATE(MAX(created_at)) FROM fund_ranking) {condition}
                 ORDER BY id
                 LIMIT @PageSize OFFSET @PageNumber";
             var result = await _connection.QueryAsync<FundRankingEntity>(sql, new { PageNumber = pageNumber, PageSize = model.PageSize });
